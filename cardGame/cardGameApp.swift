4e05866c310c03d30de9ff157472f9785d5b0785
roshanmagar <roshanmgr130@gmail.com>
2023-05-07 11:37:41 +0545

    Initial Commit

---
+++ cardGame/cardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardGameApp.swift
+//  cardGame
+//
+//  Created by Roshan Magar on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct cardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
